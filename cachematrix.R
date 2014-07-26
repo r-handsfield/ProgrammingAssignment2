604b3818955e461be5d33651473fa62400e63804
r-handsfield <r.handsfield@gmail.com>
2014-07-26 18:51:56 -0400

    modifies ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -90,3 +90,33 @@ cacheSolve <- function(x, ...) {
         x$setinverse(I)     	# bind the inverse to the matrix object
         I 		     	# return the inverse
 }
+
+
+
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
